c24abbfa9a3085263f836c348e5a584b36f1aad7
Daniel Chong <danchong625@gmail.com>
2018-01-11 22:56:22 -0800

    [feat] adds route to test page

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
 
 import HomePage from './components/HomePage.jsx';
+import TestPage from './components/TestPage.jsx';
 
 class Routes extends Component {
   constructor() {
@@ -11,6 +12,7 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/test" component={TestPage} />
         <Route path="/" component={HomePage} />
       </Switch>
     )
