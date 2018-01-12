9c9b102b8b3c2175488f6a285222b166d7abed5c
Daniel Chong <danchong625@gmail.com>
2018-01-11 22:55:40 -0800

    [feat] adds listeners to watch for load and close

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const electron = require('electron');
-const { app, BrowserWindow } = electron;
-const path = require('path');
+const { app, BrowserWindow } = require('electron');
 
 app.on('ready', () => {
   let win = new BrowserWindow({ width: 1280, height: 800 });
-  win.loadURL(path.resolve(__dirname, './dist/index.html'));
+  win.loadURL(`file://${__dirname}/dist/index.html`);
+  win.webContents.on('did-finish-load', () => {
+    if (!win) {
+      throw new Error('"win" is not defined');
+    }
+
+  win.show();
+  win.focus();
+  });
+
+  win.on('closed', () => {
+    win = null;
+  });
 });
