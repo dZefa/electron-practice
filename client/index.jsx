8b0b738852d45d080e9f78045550bba02fcb7f6f
Daniel Chong <danchong625@gmail.com>
2018-01-11 22:55:54 -0800

    [feat] implements redux and router

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import { configureStore, history } from './src/store/configureStore';
 
 import App from './src/app.jsx';
 
+const store = configureStore();
+
 render(
-  <App />,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
